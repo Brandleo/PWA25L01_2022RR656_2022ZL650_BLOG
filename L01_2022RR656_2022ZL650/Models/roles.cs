1b553d68b9db1373a526a655918f748f6569112f
Paquix <jfzluna@gmail.com>
2025-02-18 18:28:50 -0600

    Modelos de roles,usuarios y publicaciones

---
+++ L01_2022RR656_2022ZL650/Models/roles.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace L01_2022RR656_2022ZL650.Models
+{
+    public class roles
+    {
+        [Key]
+        public int rolId { get; set; }
+        public string rol {  get; set; }
+    }
+}
---
+++ L01_2022RR656_2022ZL650/Models/usuarios.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace L01_2022RR656_2022ZL650.Models
+{
+    public class usuarios
+    {
+        [Key]
+        public int usuarioId { get; set; }
+        public int rolId { get; set; }
+        public string nombreUsuario { get; set; }
+        public string clave { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+    }
+}
