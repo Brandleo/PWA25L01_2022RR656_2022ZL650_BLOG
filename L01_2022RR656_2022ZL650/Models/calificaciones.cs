6ee8ec5201f2e43e12c964c3029aa2a51e40e815
Brandleo <brandon.repreza@catolica.edu.sv>
2025-02-18 18:21:39 -0600

    Creación de modelos para comentarios y calificaciones

---
+++ L01_2022RR656_2022ZL650/Models/calificaciones.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+
+
+namespace L01_2022RR656_2022ZL650.Models
+{
+    public class calificaciones
+    {
+
+        public int calificacionId { get; set; }
+
+        public int ? publicacionId { get; set; }
+
+        public int? usuarioId { get; set; }
+
+
+        public int ? calificacion {  get; set; }
+
+    }
+}
---
+++ L01_2022RR656_2022ZL650/Models/comentarios.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+
+namespace L01_2022RR656_2022ZL650.Models
+{
+    public class comentarios
+    {
+
+        [Key]
+
+        public int cometarioId { get; set; }
+
+        public int ? publicacionId { get; set; }
+
+        public string comentario { get; set; }
+
+        public int? usuarioId {  get; set; }
+
+
+
+
+    }
+}
