94fc287f2cd2bbff089edd7ca443a62b9b8e58e7
Paquix <jfzluna@gmail.com>
2025-02-18 18:34:18 -0600

    DbSet roles usuarios y publicaciones

--- L01_2022RR656_2022ZL650/Models/blogDBContext.cs
+++ L01_2022RR656_2022ZL650/Models/blogDBContext.cs
@@ -23,6 +23,9 @@ namespace L01_2022RR656_2022ZL650.Models
         public DbSet<comentarios> comentarios { get; set; }
 
         public DbSet<calificaciones> calificaciones { get; set; }
+        public DbSet<roles> roles { get; set; }
+        public DbSet<usuarios> usuarios { get; set; }
+        public DbSet<publicaciones> publicaciones { get; set; }
 
     }
 }
